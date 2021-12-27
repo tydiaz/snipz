6bacb47bb1ee4fda946ab4c8fc2f0614d40d2ac0
Ty <nodedev21@protonmail.com>
2021-12-27 18:00:08 -0500

    add shared module and shared modal component
    add functionality and interactivity to modals start refactor service

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private modals: IModal[] = [];
+
+  constructor() {}
+
+  isModalOpen() {
+    return true;
+  }
+
+  toggleModal() {
+    // this.visible = !this.visible;
+  }
+
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false,
+    });
+    console.log(this.modals);
+  }
+}
